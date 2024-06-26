972baea1acd149027103d1e65ed88097cbbb84cc
ZhenhuiHe15 <herbertynwa@outlook.com>
2024-06-26 14:08:31 +1000

    create website

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+import Logo from './Logo';
+import ThemeToggler from './ThemeToggler';
+const Header = () => {
+  return (
+    <header>
+      <ThemeToggler />
+    </header>
+  );
+};
+
+export default Header;
