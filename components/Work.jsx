42d28bdce6727221b3e0ee61cbe4f74200f41175
ZhenhuiHe15 <herbertynwa@outlook.com>
2024-07-02 15:58:09 +1000

    add a animations for the setction title

--- components/Work.jsx
+++ components/Work.jsx
@@ -78,19 +78,18 @@ const Work = () => {
       <div className='container mx-auto'>
         {/* text */}
         <div
-          className=' max-w[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 
-        xl:h-[400px] flex flex-col justify-center xl:items-start'>
+          className='max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center
+xl:items-start'>
           <h2 className='section-title mb-4 '>Latest Project</h2>
           <p className='subtitle mb-8'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error,
-            ullam.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. ullam.
           </p>
           <Link href='/projects'>
             <Button>All projects</Button>
           </Link>
         </div>
         {/* slider */}
-        <div className='xl:max-w-[1000px] xl:absolute right-0 top-0'>
+        <div className='xl:max-w-[1000px] xl:absolute right-20 top-0'>
           <Swiper
             className='h-[480px]'
             slidesPerView={1}
